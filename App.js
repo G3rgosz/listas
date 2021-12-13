ffd6f38a60562a993c5565b39e9372d9f3000a2a
Gergosz-2000 <gergoszivak@gmail.com>
2021-12-13 11:18:15 +0100

    kesz

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import {useState} from 'react';
+import { Button } from 'react-native-web';
+
 
 export default function App() {
+
+  const [employees, setEmployees] = useState([]);
+  const url = 'http://localhost:3000/employees';
+
+  function handleFetchButton(){
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      //console.log(result);
+      setEmployees(result);
+    });
+  }
+
+  function renderItem({item}){
+    return(
+      <View>
+        <Text>{item.name} {item.city} {item.salary}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lista</Text>
+      <Button title="Letölt" onPress={handleFetchButton}/>
+      <FlatList 
+        data={employees}
+        renderItem={renderItem}
+        keyExtractor={item => item.name}
+      />
       <StatusBar style="auto" />
     </View>
   );
